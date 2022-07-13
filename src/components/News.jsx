849dfe4758781da37fcc83b6b80f5b232a1bd5ad
SahilRaj1 <raj24sahil@gmail.com>
2022-07-14 01:51:56 +0530

    commit

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -3,84 +3,34 @@ import NewsItems from './NewsItems'
 
 export default class News extends Component {
 
-  articles = [
-    {
-      "source": {
-        "id": "bleacher-report",
-        "name": "Bleacher Report"
-      },
-      "author": null,
-      "title": "⭐ &#x27;The Champions&#x27; Season Finale ⭐",
-      "description": "Fan easier, fan faster and fan better with Bleacher Report. Keep up with the latest storylines, expert analysis, highlights and scores for all your favorite sports.",
-      "url": "https://bleacherreport.com/videos/244201-the-champions-season-6-episode-3",
-      "urlToImage": null,
-      "publishedAt": "2022-06-15T17:37:27.3831502Z",
-      "content": null
-    },
-    {
-      "source": {
-        "id": "bleacher-report",
-        "name": "Bleacher Report"
-      },
-      "author": null,
-      "title": "Live Bradley Beal Interview ",
-      "description": "Fan easier, fan faster and fan better with Bleacher Report. Keep up with the latest storylines, expert analysis, highlights and scores for all your favorite sports.",
-      "url": "https://bleacherreport.com/videos/244886-taylor-rooks-x-bradley-beal",
-      "urlToImage": null,
-      "publishedAt": "2022-06-15T17:22:35.1827416Z",
-      "content": null
-    },
-    {
-      "source": {
-        "id": "usa-today",
-        "name": "USA Today"
-      },
-      "author": null,
-      "title": "Daily Briefing",
-      "description": "The day's top stories, from sports to movies to politics to world events.",
-      "url": "https://profile.usatoday.com/newsletters/daily-briefing/",
-      "urlToImage": "https://profile.usatoday.com/newsletters/resources/usat/property/usatoday/newsletter-thumbs/8872UT-E-NLETTER02@2x.jpg",
-      "publishedAt": "2021-08-15T15:35:07+00:00",
-      "content": "The day's top stories, from sports to movies to politics to world events."
-    },
-    {
-      "source": {
-        "id": "the-washington-times",
-        "name": "The Washington Times"
-      },
-      "author": "The Washington Times http://www.washingtontimes.com",
-      "title": "Latest Quizzes",
-      "description": "Take a break from the hard news of the day and enjoy a quiz on entertainment, sports, history and politics only from The Washington Times.",
-      "url": "https://www.washingtontimes.com/quiz/",
-      "urlToImage": null,
-      "publishedAt": "2021-02-10T03:52:37.2719772Z",
-      "content": "Featured Quizzes\r\nAttention all William Shakespeare experts. Pinpoint the prose's origin plucked from one of his many famous plays in this multiple-choice challenge.\r\n Shares \r\nName these legendary c… [+32652 chars]"
-    }
-  ]
-
   constructor() {
     super();
     this.state = {
-      articles: this.articles,
+      articles: [],
       loading: false
     }
+  }
+
+  async componentDidMount() {
+    const url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=9560ffedb2164314aab25c878757e27e";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({articles:  parsedData.articles});
+    console.log(this.articles);
   }
 
   render() {
     return (
       <>
-        <div className="cotnainer my-3 mx-4">
-          <h1>Top Headlines</h1>
-          <div className='container row'>
-            <div className="col-md-3">
-              <NewsItems title="myTitle" description="myDesc" imageUrl="https://profile.usatoday.com/newsletters/resources/usat/property/usatoday/newsletter-thumbs/8872UT-E-NLETTER02@2x.jpg" newsUrl="TODO" />
-            </div>
-            <div className="col-md-3">
-              <NewsItems title="myTitle" description="myDesc" />
-            </div>
-            <div className="col-md-3">
-              <NewsItems title="myTitle" description="myDesc" />
-            </div>
+        <div className="cotnainer my-3 mx-4" >
+          <h1 style={{ textAlign: "center" }}>Top Headlines</h1>
+          <div className='row' style={{ margin: "auto" }}>
+            {this.state.articles.map((element) => {
+              return <div className="col-md-4" style={{ align: "center" }}>
+                <NewsItems key={element.url} title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url} />
+              </div>
+            })}
           </div>
         </div>
       </>
--- src/components/NewsItems.jsx
+++ src/components/NewsItems.jsx
@@ -7,12 +7,12 @@ export default class NewsItems extends Component {
 
         return (
             <div className='my-3'>
-                <div className="card my-3" style={{ width: "18rem;" }}>
-                    <img src={imageUrl} className="card-img-top" alt="..." />
+                <div className="card my-3" style={{ width: "27rem" }}>
+                    <img src={imageUrl?imageUrl:"https://slidesmania.com/wp-content/uploads/2021/01/Newspapers-789x444.jpg"} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
-                        <a href="/newsdetail/" className="btn btn-primary">Go somewhere</a>
+                        <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-primary">Read More</a>
                     </div>
                 </div>
             </div>
