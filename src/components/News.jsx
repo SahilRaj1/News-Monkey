1bf966cbb4dd99015d87768c8bba152ede6818ab
SahilRaj1 <raj24sahil@gmail.com>
2022-07-14 19:32:00 +0530

    commit

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import NewsItems from './NewsItems'
+import Spinner from './Spinner'
 
 export default class News extends Component {
 
@@ -7,30 +8,65 @@ export default class News extends Component {
     super();
     this.state = {
       articles: [],
-      loading: false
+      loading: false,
+      page: 1
     }
   }
 
   async componentDidMount() {
-    const url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=9560ffedb2164314aab25c878757e27e";
+    const url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=9560ffedb2164314aab25c878757e27e&page=${this.state.page}&pageSize=3`;
     let data = await fetch(url);
+    this.setState({loading: true})
     let parsedData = await data.json();
-    console.log(parsedData);
-    this.setState({articles:  parsedData.articles});
-    console.log(this.articles);
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+      loading: false
+    });
+  }
+
+  handlePrevClick = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=9560ffedb2164314aab25c878757e27e&page=${this.state.page - 1}&pageSize=3`;
+    let data = await fetch(url);
+    this.setState({loading: true})
+    let parsedData = await data.json();
+    this.setState({
+      loading: false,
+      page: this.state.page - 1,
+      articles: parsedData.articles
+    });
+  }
+
+  handleNextClick = async () => {
+    if (this.state.page + 1 <= Math.ceil(this.state.totalResults / 3)) {
+      const url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=9560ffedb2164314aab25c878757e27e&page=${this.state.page + 1}&pageSize=3`;
+      let data = await fetch(url);
+    this.setState({loading: true})
+    let parsedData = await data.json();
+      this.setState({
+        loading: false,
+        page: this.state.page + 1,
+        articles: parsedData.articles
+      });
+    }
   }
 
   render() {
     return (
       <>
         <div className="cotnainer my-3 mx-4" >
-          <h1 style={{ textAlign: "center" }}>Top Headlines</h1>
+          <h1 style={{ textAlign: "center" }}>News Monkey - Top Headlines</h1>
+          {this.state.loading && <Spinner/>}
           <div className='row' style={{ margin: "auto" }}>
-            {this.state.articles.map((element) => {
-              return <div className="col-md-4" style={{ align: "center" }}>
-                <NewsItems key={element.url} title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url} />
+            {!(this.state.loading) && this.state.articles.map((element) => {
+              return <div className="col-md-4" style={{ margin: "auto", display: "flex", justifyContent: "center" }}>
+                <NewsItems key={element.url} title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
               </div>
             })}
+          </div>
+          <div className="container" style={{ margin: "auto", display: "flex", justifyContent: "center" }}>
+            <button type="button" className="btn btn-dark mx-3 my-2" disabled={this.state.page <= 1} onClick={this.handlePrevClick}>&larr; Previous</button>
+            <button type="button" className="btn btn-dark mx-3 my-2" disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / 3)} onClick={this.handleNextClick}>Next &rarr;</button>
           </div>
         </div>
       </>
--- src/components/NewsItems.jsx
+++ src/components/NewsItems.jsx
@@ -7,12 +7,12 @@ export default class NewsItems extends Component {
 
         return (
             <div className='my-3'>
-                <div className="card my-3" style={{ width: "27rem" }}>
+                <div className="card my-3" style={{ width: "27rem"}}>
                     <img src={imageUrl?imageUrl:"https://slidesmania.com/wp-content/uploads/2021/01/Newspapers-789x444.jpg"} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
-                        <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-primary">Read More</a>
+                        <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-dark">Read More</a>
                     </div>
                 </div>
             </div>
