d4d8364a42ee3ed47ebd6edcc54677411d6202b9
SahilRaj1 <raj24sahil@gmail.com>
2022-07-17 19:33:30 +0530

    commit

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -5,7 +5,7 @@ export default class spinner extends Component {
   render() {
     return (
       <div className='text-center'>
-        <img src={loading} alt="loading..." srcset="" />
+        <img src={loading} alt="loading..." srcSet="" />
       </div>
     )
   }
